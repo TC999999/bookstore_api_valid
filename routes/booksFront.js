1bac2755cc3fa255ef295e48405331626d3afc50
Tyler <insertcleverusernamehere999@gmail.com>
2024-03-06 11:14:40 -0500

    finished up basic front end

--- routes/booksFront.js
+++ routes/booksFront.js
@@ -25,7 +25,6 @@ router.get("/add/", async (req, res, next) => {
 router.get("/:isbn/", async (req, res, next) => {
   try {
     const results = await axios.get(`${baseURL}/${req.params.isbn}`);
-    // console.log("results:", results);
     const book = results.data.book;
     return res.render("bookData.html", { book });
   } catch (err) {
@@ -42,6 +41,39 @@ router.post("/add/", async (req, res, next) => {
     data.pages = parseInt(data.pages);
 
     await axios.post(`${baseURL}`, data);
+    return res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/update/:isbn", async (req, res, next) => {
+  try {
+    const results = await axios.get(`${baseURL}/${req.params.isbn}`);
+    const book = results.data.book;
+    return res.render("updateBookForm.html", { book });
+  } catch (err) {
+    return next(
+      new ExpressError(`There is no book with isbn ${req.params.isbn}`, 404)
+    );
+  }
+});
+
+router.post("/update/:isbn", async (req, res, next) => {
+  try {
+    const data = req.body;
+    data.year = parseInt(data.year);
+    data.pages = parseInt(data.pages);
+    await axios.put(`${baseURL}/${req.params.isbn}`, data);
+    return res.redirect(`/${req.params.isbn}`);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/delete/:isbn", async (req, res, next) => {
+  try {
+    await axios.delete(`${baseURL}/${req.params.isbn}`);
     return res.redirect("/");
   } catch (err) {
     return next(err);
